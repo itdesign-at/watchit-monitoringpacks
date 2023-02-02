a658b7605e651b57e73c6fffc41702a8fb06d6d1
Werner Neunteufl <100945666+itd-neunteufl@users.noreply.github.com>
2023-02-02 14:28:09 +0100

    add Windows Services

---
+++ SNMP/windows-services.php
@@ -0,0 +1,52 @@
+#!/usr/bin/env php
+<?php
+
+require_once("/opt/watchit/sources/php/vendor/autoload.php");
+
+use ITdesign\Plugins\Constants;
+use ITdesign\Plugins\StateCorrelation;
+use ITdesign\Utils\CommandLine;
+use ITdesign\Net\Snmp;
+
+const WindowsService = "Windows Service";
+
+if (!isset($OPT)) {
+    $OPT = CommandLine::getCommandLineOptions($argv);
+}
+
+$keyword = $OPT['k'] ?? 'windowsServicesTable';
+$host = $OPT['h'] ?? '';
+$service = $OPT['s'] ?? '';
+$debug = $OPT['Debug'] ?? false;
+
+CommandLine::terminateOnEmpty($host);
+
+$snmp = new Snmp($host);
+$snmp->setDebug($debug);
+
+$winServices = $snmp->getWindowsServices();
+
+if (count($winServices) < 1) {
+    print Constants::NoDataViaSNMP . "\n";
+    exit(Constants::NUMERIC_UNKNOWN);
+}
+
+$customTable = new StateCorrelation(['k' => $keyword, 'h' => "$host", 's' => "$service", 'Debug' => $debug]);
+$customTable->setHeader([WindowsService]);
+
+foreach ($winServices as $s) {
+    $customTable->add([WindowsService => $s]);
+}
+
+$customTable->set("Text", sprintf("%d Services", count($winServices)));
+$customTable->bye();
+
+
+
+
+
+
+
+
+
+
