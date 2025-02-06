e64c0ee37762db8b85f17db4f72266d86db99201
itd-kohlmann <117370072+itd-kohlmann@users.noreply.github.com>
2025-02-06 10:21:23 +0100

    Update Get-LockedOutUsers.ps1

--- AD LOCKED OUT USERS/Get-LockedOutUsers.ps1
+++ AD LOCKED OUT USERS/Get-LockedOutUsers.ps1
@@ -1,12 +1,12 @@
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Get all locked-out users from the specified server with the provided credentials
-$lockedUsers = Search-ADAccount -LockedOut -Server domaincontrollerhiereinfügen | Select-Object Name, SamAccountName
+# Get all locked-out users
+$lockedUsers = Search-ADAccount -LockedOut -Server DC01 | Select-Object SamAccountName
 
-# Convert to JSON format and output
-if ($lockedUsers) {
-    $lockedUsers | ConvertTo-Json -Depth 2
-} else {
-    Write-Output "[]"
-}
+# Convert to JSON format
+$jsonOutput = $lockedUsers | ConvertTo-Json -Depth 2
+
+# Output the JSON string without extra newline characters
+Write-Output $jsonOutput
+
